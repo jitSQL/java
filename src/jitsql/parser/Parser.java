b8c78b0a7b10fc8b5e34ea50d54f076b5857f3be
Shiyuan Guo <sguo35@gmail.com>
2018-07-15 13:31:54 -0700

    Remove I/O from testing

--- src/jitsql/parser/Parser.java
+++ src/jitsql/parser/Parser.java
@@ -13,10 +13,10 @@ public class Parser {
 	public static void main(String[] args) throws IOException {
 		
 		// Take input
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		// Convert to a char array for efficiency
-		char[] cmd = reader.readLine().toCharArray();
+		//char[] cmd = reader.readLine().toCharArray();
 		
 		// We use a Trie to pattern match reserved words
 		Trie data = new Trie();
@@ -42,65 +42,72 @@ public class Parser {
 		int start = 0;
 		int end = 0;
 		long startTime = System.currentTimeMillis();
+		
+		ArrayList<String> queries = new ArrayList<String>();
+		for(int i = 0;i < 10000000; i++) {
+			queries.add("SELECT \"id\" FROM");
+		}
+		for(String query: queries) {
+			char[] cmd = query.toCharArray();
 		// Continue looping through the entire input until we parse all of it
-		while (start < cmd.length && end < cmd.length) {
-			//System.out.println(cmd.length);
-			//System.out.println("STARTNUM: " + start);
-			//System.out.println("ENDNUM: " + end);
-			
-			//System.out.println(cmd[start]);
-			// Edge case checker for strings - non reserved words
-			if (cmd[start] == '"') {
-				while (true) {
-					//System.out.println("START " + cmd[start]);
-					//System.out.println("END " + cmd[end]);
-					if (cmd[end] == '"') {
-						end++;
-						break;
-					}
-					else end++;
-				}
-				// don't include quotations
-				matches.add(new Tuple(start + 1, end - 1, 5));
-				//System.out.println(matches.toString());
-			} else {
-				// look for reserved words
+			while (start < cmd.length && end < cmd.length) {
+				//System.out.println(cmd.length);
+				//System.out.println("STARTNUM: " + start);
+				//System.out.println("ENDNUM: " + end);
 				
-				boolean match = false;
-				
-				while (!match) {	
-					String build = "";
-					// Inefficient - build our search string
-					for (int i = start; i < end; i++) {
-						if (end > cmd.length) {
-							match = true;
+				//System.out.println(cmd[start]);
+				// Edge case checker for strings - non reserved words
+				if (cmd[start] == '"') {
+					while (true) {
+						//System.out.println("START " + cmd[start]);
+						//System.out.println("END " + cmd[end]);
+						if (cmd[end] == '"') {
+							end++;
 							break;
 						}
-						build += cmd[i];
+						else end++;
 					}
-					//System.out.println("BUILD: " + build);
-					if (build.equals(" ")) {
-						break;
-					}
-					// search for the string in the trie
-					if (data.search(build)) {
-						match = true;
-						// if we've found the string, add a new Tuple and stop looping
-						int type = h.get(build);
-						Tuple t = new Tuple(start, end, type);
-						
-						matches.add(t);
-						break;
-					} else {
-						end++;
+					// don't include quotations
+					matches.add(new Tuple(start + 1, end - 1, 5));
+					//System.out.println(matches.toString());
+				} else {
+					// look for reserved words
+					
+					boolean match = false;
+					
+					while (!match) {	
+						String build = "";
+						// Inefficient - build our search string
+						for (int i = start; i < end; i++) {
+							if (end > cmd.length) {
+								match = true;
+								break;
+							}
+							build += cmd[i];
+						}
+						//System.out.println("BUILD: " + build);
+						if (build.equals(" ")) {
+							break;
+						}
+						// search for the string in the trie
+						if (data.search(build)) {
+							match = true;
+							// if we've found the string, add a new Tuple and stop looping
+							int type = h.get(build);
+							Tuple t = new Tuple(start, end, type);
+							
+							matches.add(t);
+							break;
+						} else {
+							end++;
+						}
 					}
 				}
+				// Increment to the next char
+				start = end;
+				end = start + 1;
 			}
-			// Increment to the next char
-			start = end;
-			end = start + 1;
 		}
-		
 		System.out.println(matches.toString());
 		System.out.println("Took " + (System.currentTimeMillis() - startTime) + " ms");
 	}
